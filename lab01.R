9d2345ed961dae969f4bafe845ccf18cc30251b3
Brendan Zuk <zukbrend@msu.edu>
2020-01-14 16:06:23 -0500

    Finished exercise 1b

--- lab01.R
+++ lab01.R
@@ -30,22 +30,33 @@ scatter2 <- ggplot(
   )
 ) + geom_point()
 
+scatter2
+
 # scatter2 is not a useful plot because there is no related relationship between
 # the two variables. The class generally dictates what drv the vehicle can have,
 # and there are far too few data points to make any reasonable conclusions
+
+### Exercise 1b ###
 
 scatter3 <- ggplot(
   data=mpgData,
   mapping=aes(
     x=displ,
     y=hwy,
-    
+    color=class
   )
 ) + 
   geom_point() +
   xlab("Engine Size (Liters)") + 
   ylab("Highway Fuel Efficiency (mpg)") + 
   ggtitle("Exercise 1: Engine Size Vs. Highway Fuel Efficiency")
+
+scatter3
+
+# Given the scatter plot generated with color coded dots according to the type
+# of vehicle, it appears that 2-seat vehicles are outlyers in the sense that they
+# achieve better highway fuel efficiency than other types of vehicles with the
+# same size engine
 
 
 
