b51c9972c08251b727db5fc41d7a9971e80f3547
Brendan Zuk <zukbrend@msu.edu>
2020-01-27 20:21:59 -0500

    Finished in-class exercise 1

--- InClassExercise1.R
+++ InClassExercise1.R
@@ -15,38 +15,39 @@ intuitiveBalanceModel <- lm(
 summary(intuitiveBalanceModel)
 
 anova(intuitiveBalanceModel, fullBalanceModel)
-# This ANOVA test yields an F value of 6.1, thus there are more factors that meaningfully 
-# contribute to predicting balance
+# This ANOVA test yields an F value of 6.1 and a p value of 2.2e-16, thus all of 
+# the intuitive variables we picked well predict balance. 
 
-intuitiveBalanceModel2 <- lm(
+updatedIntuitiveBalanceModel <- lm(
   balance ~ age + default + marital + job + education,
   data=bank
 )
-summary(intuitiveBalanceModel2)
+summary(updatedIntuitiveBalanceModel)
 
-anova(intuitiveBalanceModel2, fullBalanceModel)
-# This ANOVA test yields an F value of 7.8, thus we got farther from accurately 
-# predicting balance by adding job and education, so at least one of these factors 
-# is irrelevant in predicting balance
+anova(updatedIntuitiveBalanceModel, fullBalanceModel)
+# This ANOVA test yields an F value of 7.8, which tells us that this model better 
+# predicts balance than the first intuitive model we created. This ANOVA test also 
+# yields a p value of 2.2e-16, thus the additional variables of job and education 
+# that were added to this model both help better predict balance.
 
 # looking at the t value column of the summary of fullBalanceModel, I chose all 
 # categories with a t value > 2
-intuitiveBalanceModel3 <- lm(
-  balance ~ age + default + marital + job + loan + month,
+statisticallyImportantModel <- lm(
+  balance ~ age + default + marital + loan,
   data=bank
 )
-summary(intuitiveBalanceModel3)
+summary(statisticallyImportantModel)
 
-anova(intuitiveBalanceModel3, fullBalanceModel)
-# This ANOVA test yields an F value of 1.2, thus we find that this model is a good 
-# predictor of balance after removing meaningless variables
+anova(statisticallyImportantModel, fullBalanceModel)
+# This ANOVA test yields an F value of 5.7 and a p value of 2.2e-16, thus we find that 
+# this model is once again, a good predictor, but not as good of a predictor. 
 
-goodPredictor <- intuitiveBalanceModel3
+
+goodPredictor <- updatedIntuitiveBalanceModel
 
 # The resulting final model demonstrates that the age of the customer, whether the 
 # customer has any credit in default, the marital status of the customer, what job 
-# the customer holds, whether the customer holds a loan or not, and what month the 
-# customer joined the bank in, all hold meaningful correlations when attempting to 
-# predict the balance a customer has in an account with the bank. Which further leads
-# to a model which can also be useful in predicting any of the aforementioned variables, 
-# as correlations work in both directions. 
+# the customer holds, and the education level of the customer, all hold meaningful 
+# correlations when attempting to predict the balance a customer has in an account 
+# with the bank. Which further leads to a model which can also be useful in predicting 
+# any of the aforementioned variables, as correlations work in both directions. 
